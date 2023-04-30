73dc23be08e76f2bdfb8c51d3e60e8eefe8d032b
Rajat <rajat@tiobu.com>
2023-04-30 17:47:27 +0530

    Initial setup

---
+++ mother-kitchen-frontend/src/App.jsx
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+    <div className="text-3xl font-bold w-screen h-screen flex justify-center items-center">Mother Kitchen Project</div>
+  )
+}
+
+export default App
