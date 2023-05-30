aec6ccdf618e844b1664aea8f6a1b7640752d45c
Rajat <rajat@tiobu.com>
2023-05-30 22:14:13 +0530

    Minor UI fixes

--- mother-kitchen-frontend/src/components/Brand/Brand.jsx
+++ mother-kitchen-frontend/src/components/Brand/Brand.jsx
@@ -6,8 +6,8 @@ const Brand = () => {
   return (
     <BorderDiv className="mx-7 bg-[#516AC0] p-5 md:mx-14 md:p-10 lg:mx-24">
       <h4 className=" mx-auto text-center font-gluten text-[8px] leading-4 text-white md:w-[80%] md:text-xl">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod massa est, vitae aliquet mi egestas in.
-        Curabitur fermentum lorem eget dolor molestie accumsan. Fusce ut metus vitae nibh bibendum facilisis.
+        We&apos;re a group of companies transforming the FMCG industry through healthy and innovative products,
+        prioritizing your well-being and the planet.
       </h4>
       <div className="m-5 grid grid-cols-2 gap-8 md:m-10 md:grid-cols-3 md:gap-x-10 md:gap-y-10 lg:m-20 lg:gap-x-24 lg:gap-y-14">
         <ImageBox image={TextLogo} className="bg-[#7E97ED] p-6 md:px-8" clickable={true} />
