0addde044024fd45bfc3e96639dfae008bc36947
Rajat <rajat@tiobu.com>
2023-05-30 20:13:08 +0530

    Minor UI fixes

--- mother-kitchen-frontend/src/components/custom/BorderDiv.jsx
+++ mother-kitchen-frontend/src/components/custom/BorderDiv.jsx
@@ -1,7 +1,7 @@
 const BorderDiv = ({ children, className }) => {
   return (
     <div
-      className={`relative -z-20 rounded-2xl border-4 border-b-8 border-r-8 border-t-8 border-black shadow-2xl before:absolute before:left-[0.8px] before:top-[0.8px] before:h-full before:w-full before:rounded-[0.5rem] before:border-[3px] before:border-white md:rounded-[2rem] md:border-8 md:border-r-[12px] md:border-t-[12px] ${className}`}
+      className={`relative -z-20 rounded-2xl border-4 border-b-8 border-r-8 border-t-8 border-black shadow-2xl before:absolute before:left-[0.8px] before:top-[0.8px] before:h-full before:w-full before:rounded-[0.5rem] before:border-[3px] before:border-white md:rounded-[2rem] md:border-8 md:border-r-[12px] md:border-t-[12px] before:md:rounded-[1.4rem] ${className}`}
     >
       {children}
     </div>
