08f181d52fd5d67057d386ddc83edea2ad4bce9e
Rajat <rajat@tiobu.com>
2023-05-30 22:10:06 +0530

    Minor UI fixes

--- mother-kitchen-frontend/src/components/custom/ImageBox.jsx
+++ mother-kitchen-frontend/src/components/custom/ImageBox.jsx
@@ -1,7 +1,7 @@
 const ImageBox = ({ image, className, clickable }) => {
   return (
     <div
-      className={`relative overflow-hidden rounded-xl border-2 border-black shadow-2xl before:absolute before:h-full before:w-full before:border before:border-white md:rounded-[2rem] md:border-4 md:border-b-8 md:border-r-8 before:md:rounded-[1.7rem] before:md:border-2 ${
+      className={`relative overflow-hidden rounded-lg border-2 border-black shadow-2xl before:absolute before:h-full before:w-full before:rounded-lg before:border before:border-white md:rounded-[2rem] md:border-4 md:border-b-8 md:border-r-8 before:md:rounded-[1.7rem] before:md:border-2 ${
         clickable ? 'cursor-pointer duration-300 hover:scale-95' : ''
       }`}
     >
