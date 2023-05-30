9bd6ff0a536ff11ed394ae3b28245b981e27c1bd
Rajat <rajat@tiobu.com>
2023-05-30 20:51:58 +0530

    Clickable Brand Image box

--- mother-kitchen-frontend/src/components/Brand/Brand.jsx
+++ mother-kitchen-frontend/src/components/Brand/Brand.jsx
@@ -4,22 +4,20 @@ import ImageBox from '../custom/ImageBox'
 
 const Brand = () => {
   return (
-    <div>
-      <BorderDiv className="mx-7 bg-[#516AC0] p-5 md:mx-14 md:p-10 lg:mx-24">
-        <h4 className=" mx-auto text-center font-gluten text-[8px] leading-4 text-white md:w-[80%] md:text-xl">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod massa est, vitae aliquet mi egestas
-          in. Curabitur fermentum lorem eget dolor molestie accumsan. Fusce ut metus vitae nibh bibendum facilisis.
-        </h4>
-        <div className="m-5 grid grid-cols-2 gap-8 md:m-10 md:grid-cols-3 md:gap-x-10 md:gap-y-10 lg:m-20 lg:gap-x-24 lg:gap-y-14">
-          <ImageBox image={TextLogo} className="bg-[#7E97ED] p-6 md:px-8" clickable={true} />
-          <ImageBox image={TextLogo} className="bg-[#7E97ED] p-6 md:px-8 md:py-16" clickable={true} />
-          <ImageBox image={TextLogo} className="bg-[#7E97ED] p-6 md:px-8 md:py-16" clickable={true} />
-          <ImageBox image={TextLogo} className="bg-[#7E97ED] p-6 md:px-8 md:py-16" clickable={true} />
-          <ImageBox image={TextLogo} className="bg-[#7E97ED] p-6 md:px-8 md:py-16" clickable={true} />
-          <ImageBox image={TextLogo} className="bg-[#7E97ED] p-6 md:px-8 md:py-16" clickable={true} />
-        </div>
-      </BorderDiv>
-    </div>
+    <BorderDiv className="mx-7 bg-[#516AC0] p-5 md:mx-14 md:p-10 lg:mx-24">
+      <h4 className=" mx-auto text-center font-gluten text-[8px] leading-4 text-white md:w-[80%] md:text-xl">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod massa est, vitae aliquet mi egestas in.
+        Curabitur fermentum lorem eget dolor molestie accumsan. Fusce ut metus vitae nibh bibendum facilisis.
+      </h4>
+      <div className="m-5 grid grid-cols-2 gap-8 md:m-10 md:grid-cols-3 md:gap-x-10 md:gap-y-10 lg:m-20 lg:gap-x-24 lg:gap-y-14">
+        <ImageBox image={TextLogo} className="bg-[#7E97ED] p-6 md:px-8" clickable={true} />
+        <ImageBox image={TextLogo} className="bg-[#7E97ED] p-6 md:px-8 md:py-16" clickable={true} />
+        <ImageBox image={TextLogo} className="bg-[#7E97ED] p-6 md:px-8 md:py-16" clickable={true} />
+        <ImageBox image={TextLogo} className="bg-[#7E97ED] p-6 md:px-8 md:py-16" clickable={true} />
+        <ImageBox image={TextLogo} className="bg-[#7E97ED] p-6 md:px-8 md:py-16" clickable={true} />
+        <ImageBox image={TextLogo} className="bg-[#7E97ED] p-6 md:px-8 md:py-16" clickable={true} />
+      </div>
+    </BorderDiv>
   )
 }
 
--- mother-kitchen-frontend/src/components/custom/ImageBox.jsx
+++ mother-kitchen-frontend/src/components/custom/ImageBox.jsx
@@ -2,7 +2,7 @@ const ImageBox = ({ image, className, clickable }) => {
   return (
     <div
       className={`relative overflow-hidden rounded-xl border-2 border-black shadow-2xl before:absolute before:h-full before:w-full before:border before:border-white md:rounded-[2rem] md:border-4 md:border-b-8 md:border-r-8 before:md:rounded-[1.7rem] before:md:border-2 ${
-        clickable ? '!cursor-pointer duration-300 hover:scale-95' : ''
+        clickable ? 'cursor-pointer duration-300 hover:scale-95' : ''
       }`}
     >
       <img src={image} alt="img" className={`h-full w-full object-contain ${className}`} />
