7b93f0cedee4af2a5914c83df93c2499916789b4
Rajat <rajat@tiobu.com>
2023-05-31 06:10:46 +0530

    Minor UI fixes

--- mother-kitchen-frontend/src/components/custom/NavigationButtons.jsx
+++ mother-kitchen-frontend/src/components/custom/NavigationButtons.jsx
@@ -5,7 +5,7 @@ const NavigationButton = ({ children, className, path }) => {
       to={path}
       className={`rounded-md  border-2 border-b-4 border-l-4 border-black px-8 py-3 font-lexend font-semibold text-white
       duration-300 hover:scale-95 md:rounded-xl md:border-4 md:border-b-[6px] md:border-l-[6px] ${className || ''}`}
-      onClick={() => scrollTo(0, 0)}
+      onClick={() => setTimeout(window.scrollTo(0, 0), 100)}
     >
       {children}
     </Link>
