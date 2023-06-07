01e6558cc913e3088260290a636b1812fe151aeb
Rajat <rajat@tiobu.com>
2023-06-07 22:19:45 +0530

    Added backend

---
+++ mother-kitchen-backend/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = 3002;
+app.use(
+  express.static(path.join(__dirname, '../mother-kitchen-frontend/dist'))
+);
+
+app.get('*', (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname, '../mother-kitchen-frontend/dist/index.html')
+  );
+});
+
+app.listen(PORT, () =>
+  console.log(`The application started successfully on port ${PORT}`)
+);
